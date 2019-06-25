fe480a6fda3e774c0ebd8537640928880d9c25fd
Bret Baker <bretbaker808@gmail.com>
2019-06-25 15:52:32 -0700

    game basics working, stuck on conditions preventing special characters and repeat guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,8 +3,12 @@ let guessesRemaining = 12;
 let lettersGuessed = [];
 let letterGuessed;
 let alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let charMatch = alphabet.match
 let winCount = 0;
 let lossCount = 0;
+let charTypeCheck;
+let alreadyGuessedCheck;
+
 
 // get random word
 let beachWords = ["boardwalk", "beachball", "crab", "conch", "seagull", "lifeguard", "surfboard", "pelican", "starfish", "sandcastle"];
@@ -81,6 +85,7 @@ let winner = () => {
     document.getElementById("guessesLeft").innerHTML = "12";
     lettersGuessed = [];
     document.getElementById("guesses").innerHTML = "";
+    document.getElementById("alert").innerHTML = "";
 }
 
 // create lose condition funcion
@@ -101,28 +106,62 @@ let loser = () => {
     document.getElementById("guessesLeft").innerHTML = "12";
     lettersGuessed = [];
     document.getElementById("guesses").innerHTML = "";
+    document.getElementById("alert").innerHTML = "";
+}
+// create function that checks condition of whether the key pressed is a letter or special character
+let charType = () => {
+    for (let i = 0; i < alphabet[i].length; i++) {
+        console.log(alphabet[i]);
+        if (letterGuessed === alphabet[i]) {
+            return true;
+        } else if (letterGuessed != alphabet[i]) {
+            return false;
+        }
+    }
+}
+
+// create function that checks condition of whether the letter guessed has already been guessed or not
+let alreadyGuessed = () => {
+    for (let i = 0; i < blankSpaces[i].length; i++) {
+        if (letterGuessed === blankSpaces[i]) {
+            return true;
+        } else if (letterGuessed != blankSpaces[i]) {
+            return false;
+        }
+    }
 }
 
 // get user input and return appropriate output
 document.onkeyup = (event) => {
     letterGuessed = event.key;
     letterGuessed = letterGuessed.toUpperCase();
-
-    lettersGuessed.push(letterGuessed);
-    letterMatch();
-    let winWord = blankSpaces.join("");
-    console.log(chosenWord);
-    console.log(winWord);
-    console.log(currentWord);
-    console.log(blankSpaces);
-    document.getElementById("currentWord").innerHTML = blankSpaces.join(" ");
-    document.getElementById("guesses").innerHTML = lettersGuessed.join(", ");
-    guessesRemaining -= 1;
-    document.getElementById("guessesLeft").innerHTML = guessesRemaining;
-
-    if (winWord === chosenWord && guessesRemaining > 0) {
-        winner();
-    } else if (winWord != chosenWord && guessesRemaining === 0) {
-        loser();
-    }
+    console.log(letterGuessed);
+    charType();
+    charTypeCheck = charType();
+    console.log(charTypeCheck);
+    alreadyGuessed();
+    alreadyGuessedCheck = alreadyGuessed();
+    console.log(alreadyGuessedCheck);
+    if (charTypeCheck === false) {
+        document.getElementById("alert").innerHTML = "Error! No special characters!";
+    } else if (charTypeCheck === true && alreadyGuessedCheck === false) {
+        lettersGuessed.push(letterGuessed);
+        letterMatch();
+        let winWord = blankSpaces.join("");
+        console.log(chosenWord);
+        console.log(winWord);
+        console.log(currentWord);
+        console.log(blankSpaces);
+        document.getElementById("currentWord").innerHTML = blankSpaces.join(" ");
+        document.getElementById("guesses").innerHTML = lettersGuessed.join(", ");
+        guessesRemaining -= 1;
+        document.getElementById("guessesLeft").innerHTML = guessesRemaining;
+        if (winWord === chosenWord && guessesRemaining > 0) {
+            winner();
+        } else if (winWord != chosenWord && guessesRemaining === 0) {
+            loser();
+        } 
+    } else if (charTypeCheck === true && alreadyGuessedCheck === true) {
+        document.getElementById("alert").innerHTML = "Error! Already guessed that letter!";
+    }   
 }

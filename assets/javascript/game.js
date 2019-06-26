25a29aa26a5a64a85cb91a4b71d3f89d49f40686
Bret Baker <bretbaker808@gmail.com>
2019-06-26 12:19:36 -0700

    code working, alreadyGuessedCheck working, need to create a function that checks for special characters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,12 @@
-// set initial cases
+// declare global variables
 let guessesRemaining = 12;
 let lettersGuessed = [];
 let letterGuessed;
 let alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-let charMatch = alphabet.match
 let winCount = 0;
 let lossCount = 0;
-let charTypeCheck;
-let alreadyGuessedCheck;
-
+let alreadyGuessed;
+let indices;
 
 // get random word
 let beachWords = ["boardwalk", "beachball", "crab", "conch", "seagull", "lifeguard", "surfboard", "pelican", "starfish", "sandcastle"];
@@ -19,52 +17,40 @@ let randWord = () => {
     return beachWords[randNum()];
 }
 let chosenWord = randWord();
-randWord();
-chosenWord = chosenWord.toUpperCase();
-console.log(chosenWord);
 
-// set current word
+// set random word
 let currentWord = [];
 let currentWordArr = () => {
     for (let i = 0; i < chosenWord.length; i++) {
         currentWord.push(chosenWord[i]);
     }
 }
-currentWordArr();
-console.log(currentWord);
 
-// set blank spaces for each letter of current word
+// set blank spaces for each letter of random word
 let blankSpaces = [];
 let blankSpacesArr = () => {
     for (let i = 0; i < chosenWord.length; i++) {
         blankSpaces.push("_") * i;
     }
 }
-blankSpacesArr();
 
-// create function that takes user input, compares it against all indices of currentWord and splices the value(s) into blankSpaces if match occurs
-let letterMatch = () => {
+// create function that takes user input, compares it against all indices of currentWord and splices the value(s) into blankSpaces at correct index position
+let spliceBlankSpaces = () => {
     currentWord.forEach((element) => {
-        console.log(element);
-        let indices = [];
+        // console.log(element);
+        indices = [];
         let idx = currentWord.indexOf(element);
         while (idx != -1) {
             indices.push(idx);
             idx = currentWord.indexOf(element, idx + 1);
           }
-            console.log(indices);
+            // console.log(indices);
         if (element === letterGuessed) {
             indices.forEach((i) => {
                 blankSpaces.splice(i, 1, element);
             })
         }
     })
-}
-
-// display initial blank spaces and guesses remaining
-window.onload = () => {
-    document.getElementById("currentWord").innerHTML = blankSpaces.join(" ");
-    document.getElementById("guessesLeft").innerHTML = "12";
 }
 
 // create win condition function
@@ -85,7 +71,7 @@ let winner = () => {
     document.getElementById("guessesLeft").innerHTML = "12";
     lettersGuessed = [];
     document.getElementById("guesses").innerHTML = "";
-    document.getElementById("alert").innerHTML = "";
+    document.getElementById("alert").innerHTML = "You win! Play again!";
 }
 
 // create lose condition funcion
@@ -106,62 +92,63 @@ let loser = () => {
     document.getElementById("guessesLeft").innerHTML = "12";
     lettersGuessed = [];
     document.getElementById("guesses").innerHTML = "";
-    document.getElementById("alert").innerHTML = "";
+    document.getElementById("alert").innerHTML = "You lose! Try again!";
 }
-// create function that checks condition of whether the key pressed is a letter or special character
-let charType = () => {
-    for (let i = 0; i < alphabet[i].length; i++) {
-        console.log(alphabet[i]);
-        if (letterGuessed === alphabet[i]) {
-            return true;
-        } else if (letterGuessed != alphabet[i]) {
-            return false;
-        }
+
+// create function that checks to see if letter has already been guessed, alerts the user so, and prevents them from wasting a guess
+let alreadyGuessedCheck = () => {
+    lettersGuessed.pop();
+    if (lettersGuessed.includes(letterGuessed)) {
+        lettersGuessed.push(letterGuessed);
+        return true;
+    } else {
+        lettersGuessed.push(letterGuessed);
+        return false;
     }
 }
 
-// create function that checks condition of whether the letter guessed has already been guessed or not
-let alreadyGuessed = () => {
-    for (let i = 0; i < blankSpaces[i].length; i++) {
-        if (letterGuessed === blankSpaces[i]) {
-            return true;
-        } else if (letterGuessed != blankSpaces[i]) {
-            return false;
-        }
-    }
+// call initial functions
+randWord();
+chosenWord = chosenWord.toUpperCase();
+console.log(chosenWord);
+currentWordArr();
+console.log(currentWord);
+blankSpacesArr();
+
+// display initial blank spaces and guesses remaining
+window.onload = () => {
+    document.getElementById("currentWord").innerHTML = blankSpaces.join(" ");
+    document.getElementById("guessesLeft").innerHTML = "12";
 }
 
 // get user input and return appropriate output
 document.onkeyup = (event) => {
+    document.getElementById("alert").innerHTML = "";
     letterGuessed = event.key;
     letterGuessed = letterGuessed.toUpperCase();
     console.log(letterGuessed);
-    charType();
-    charTypeCheck = charType();
-    console.log(charTypeCheck);
-    alreadyGuessed();
-    alreadyGuessedCheck = alreadyGuessed();
-    console.log(alreadyGuessedCheck);
-    if (charTypeCheck === false) {
-        document.getElementById("alert").innerHTML = "Error! No special characters!";
-    } else if (charTypeCheck === true && alreadyGuessedCheck === false) {
-        lettersGuessed.push(letterGuessed);
-        letterMatch();
+    lettersGuessed.push(letterGuessed);
+    console.log(lettersGuessed);
+    alreadyGuessedCheck();
+    alreadyGuessed = alreadyGuessedCheck();
+    if (alreadyGuessed) {
+        document.getElementById("alert").innerHTML = "You already guessed that! Guess a different letter!";
+        lettersGuessed.pop();
+    } else if (alreadyGuessed === false) {
+        spliceBlankSpaces();
         let winWord = blankSpaces.join("");
-        console.log(chosenWord);
         console.log(winWord);
-        console.log(currentWord);
+        console.log(lettersGuessed);
         console.log(blankSpaces);
         document.getElementById("currentWord").innerHTML = blankSpaces.join(" ");
         document.getElementById("guesses").innerHTML = lettersGuessed.join(", ");
         guessesRemaining -= 1;
         document.getElementById("guessesLeft").innerHTML = guessesRemaining;
-        if (winWord === chosenWord && guessesRemaining > 0) {
+        if (winWord === chosenWord) {
             winner();
         } else if (winWord != chosenWord && guessesRemaining === 0) {
             loser();
-        } 
-    } else if (charTypeCheck === true && alreadyGuessedCheck === true) {
-        document.getElementById("alert").innerHTML = "Error! Already guessed that letter!";
-    }   
+        }
+    }
+    
 }
